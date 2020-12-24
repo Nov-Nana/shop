91345c86f01fbc51381b7710dc2b020ad75cfd3f
Nov-Nana <791186022@qq.com>
2020-12-24 17:09:31 +0800

    init shop

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/*
+入口JS
+ */
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// eslint-disable-next-line
+/* eslint-disable */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
